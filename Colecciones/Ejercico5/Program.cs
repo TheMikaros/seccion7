21e812985a68ab8020d0127b82c6ea6494604092
Marco Antonio Aguayo <MikoGrenore@Outlook.com>
2022-12-27 14:05:31 -0600

    Add readme and ej5

--- Colecciones/Ejercico5/Program.cs
+++ Colecciones/Ejercico5/Program.cs
@@ -10,6 +10,44 @@ namespace Ejercico5
     {
         static void Main(string[] args)
         {
+            //5 - Usando Arrays, crea un programa que nos pida cuantos elementos tendrá el array,
+            //dependiendo de ese numero de elementos, nos ira pidiendo valores, que introduciremos
+            //en el array. una vez introducidos, nos pedirá un nuevo valor a insertar y la posición
+            //donde queremos insertarla en el array.
+
+            Console.WriteLine("Introduce la cantidad de elementos del array:");
+            int cantidad = int.Parse(Console.ReadLine());
+
+            int[] array = new int[cantidad];
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.WriteLine("Introduce el elemento " + (i + 1) + ":");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Introduce el valor a insertar:");
+            int valor = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Introduce la posición donde quieres insertar el valor:");
+            int posicion = int.Parse(Console.ReadLine());
+
+            // Desplazamos los elementos del array hacia la derecha a partir de la posición donde queremos insertar el valor
+            for (int i = array.Length - 1; i >= posicion; i--)
+            {
+                array[i + 1] = array[i];
+            }
+
+            // Insertamos el valor en la posición indicada
+            array[posicion] = valor;
+
+            // Mostramos el array resultante
+            Console.WriteLine("Array resultante:");
+            foreach (int elemento in array)
+            {
+                Console.WriteLine(elemento);
+            }
+            Console.ReadLine();
         }
     }
 }
---
+++ readme.md
@@ -0,0 +1,9 @@
+1- Crear un programa, que recorra con un bucle for del 1 al 1000, insertando en cada iteración el valor del numero en un ArrayList. una vez acabe el proceso , recorreremos ese ArrayList mostrando en consola la suma de los números  , ejemplo: 1, 3, 6, 10.
+
+2-  Pedir por pantalla 5 nombres, e insertarlo en una Lista (List) una vez insertados, se pedirá que introduzcamos un nombre, el programa tendrá que decirnos, si ese nombre esta contenido en la lista.
+
+3- Pedir por pantalla 10 números, que iremos almacenando en un Listado (o 2 como prefiramos). al terminar de introducir los números, tendremos que recorrer el/los listados, diciéndonos cuanto suman los pares y cuando los impares.
+
+4- Crear un programa nos pida introducir 10 números enteros. esos valores los introduciremos en un Listado y  mostraremos el numero mayor y menor, consultando los valores en el Listado.
+
+5- Usando Arrays, crea un programa que nos pida cuantos elementos tendrá el array, dependiendo de ese numero de elementos, nos ira pidiendo valores, que introduciremos en el array. una vez introducidos, nos pedirá un nuevo valor a insertar y la posición donde queremos insertarla en el array.
